3f3fd5c41313727f03a09d8e77b114f664e604f9
Ian Baldwin <ian@iabaldwin.com>
2013-02-11 11:33:11 -0800

    Minor updates

--- core/tests/test_iterator.cpp
+++ core/tests/test_iterator.cpp
@@ -7,29 +7,18 @@
 
 int main()
 {
+    // Load dataset
     L3::Dataset dataset( "/Users/ian/code/datasets/2012-02-06-13-15-35mistsnow/" );
-
     assert( dataset.validate() && dataset.load() );
 
+    // Build iterator
     std::string LIDAR_name = dataset.LIDAR_names[0];
-
     std::auto_ptr< L3::ConstantTimeIterator > iterator( new L3::ConstantTimeIterator( &dataset, LIDAR_name, 100.0 ) );
 
+    // Localise
     L3::Utils::localisePoseChainToOrigin( dataset.poses );
 
-    SWATHE* s = iterator->getSwathe();
-
-    SWATHE::iterator it = s->begin();
-
-    std::cout.precision(12);
-
-    //while( it != s->end() )
-    //{
-        ////std::cout << *((*it).first) << std::endl;
-        ////std::cout << *((*it).second) << std::endl;
-        //it++;
-    //}
-
+    // Run
     while( iterator->update( .1 ) )
         std::cout << iterator->relativeTime() << ":" << iterator->numScans() << std::endl;
 
--- core/tests/test_projector.cpp
+++ core/tests/test_projector.cpp
@@ -12,32 +12,19 @@ int main()
     assert( dataset.validate() && dataset.load() );
 
     // Centre poses
-    L3::Utils::localisePoseChainToOrigin( dataset.poses );
+    L3::Utils::localisePoseChainToMean( dataset.poses );
   
     // Build iterator
     std::string LIDAR_name = dataset.LIDAR_names[0];
     std::auto_ptr< L3::ConstantTimeIterator > iterator( new L3::ConstantTimeIterator( &dataset, LIDAR_name, 60.0 ) );
 
-    POSE_SEQUENCE::iterator it = dataset.poses.begin();
-
-    while( it != dataset.poses.end() )
-    {
-        //std::cout << *(*it) << std::endl;
-        it++;
-    }
-
-    // Project swathe
+    // Projector  
     std::auto_ptr<L3::Projector> projector( new L3::Projector() );
 
+    // Get swathe
     SWATHE* swathe = iterator->getSwathe();
-    SWATHE::iterator it2 = swathe->begin();
 
-    while( it2!=swathe->end() )
-    {
-        //std::cout << *((*it2).first) << std::endl;
-        it2++;
-    }
-
+    // Do Projection
     L3::PointCloudXYZ<double> cloud = projector->project( iterator->getSwathe() );
     std::cout << cloud << std::endl;
 }
