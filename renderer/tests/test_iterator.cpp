34fc28c3d76f7c39b9fb17e5f4eeb2d9b63e1a1e
Ian Baldwin <ian@iabaldwin.com>
2013-02-28 12:37:13 -0800

    Updates

--- renderer/tests/test_iterator.cpp
+++ renderer/tests/test_iterator.cpp
@@ -36,7 +36,7 @@ int main (int argc, char ** argv)
 
     L3::Visualisers::IteratorRenderer<L3::SE3> iterator_renderer( &iterator  );
     L3::Visualisers::Composite composite;
-    composite.sf = 2.0;
+    composite.sf = 4.0;
  
     composite << iterator_renderer;
     composite.current_time = dataset.start_time; 
@@ -44,6 +44,7 @@ int main (int argc, char ** argv)
     //TODO:
     //Not working because??
     top << grid << composite;
+    //top << composite << grid;
 
     win.setGLV(top);
     glv::Application::run();
