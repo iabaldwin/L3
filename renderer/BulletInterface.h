9b5f21aa9b4a43fe64a1375e1f20f5a489cdcd97
Ian Baldwin <ian@iabaldwin.com>
2013-05-14 15:55:35 -0700

    Updates

--- renderer/BulletInterface.h
+++ renderer/BulletInterface.h
@@ -19,36 +19,88 @@ namespace L3
 namespace Visualisers
 {
 
+    struct Box
+    {
+        struct bounds
+        {
+            float x,y,z;
+        };
+
+        bounds lower, upper;
+
+        Box( float x_lower, float y_lower, float z_lower, 
+                float x_upper, float y_upper, float z_upper )
+        {
+
+            lower.x = x_lower;
+            lower.y = y_lower;
+            lower.z = z_lower;
+            
+            upper.x = x_upper;
+            upper.y = y_upper;
+            upper.z = z_upper;
+        }
+        
+    };
+
+    struct BoxRenderer : L3::Visualisers::Leaf
+    {
+
+        BoxRenderer() 
+        {
+            box = new Box( -10, -10, -10, 10, 10, 10 );
+            box_shape = new btBoxShape( btVector3(10,10,10) );
+            box_body  = new btRigidBody( 10.0f, new btDefaultMotionState, box_shape, btVector3(0,0,0) );
+        }
+
+        Box* box;
+        btRigidBody* box_body;
+        btCollisionShape* box_shape;
+        
+        void onDraw3D( glv::GLV& g )
+        {
+            glv::Point3 vertices[16];
+            glv::Color  colors[16];
+
+            // Lower
+            vertices[0]( box->lower.x, box->lower.y, box->lower.z );
+            vertices[1]( box->lower.x, box->upper.y, box->lower.z );
+            vertices[2]( box->upper.x, box->upper.y, box->lower.z );
+            vertices[3]( box->upper.x, box->lower.y, box->lower.z );
+
+            glv::draw::paint( glv::draw::LineLoop, vertices, colors, 4 );
+
+            // Upper
+            vertices[0]( box->lower.x, box->lower.y, box->upper.z );
+            vertices[1]( box->lower.x, box->upper.y, box->upper.z );
+            vertices[2]( box->upper.x, box->upper.y, box->upper.z );
+            vertices[3]( box->upper.x, box->lower.y, box->upper.z );
+
+            glv::draw::paint( glv::draw::LineLoop, vertices, colors, 4 );
+       
+        }
+    };
+
     struct BulletInterface : L3::Visualisers::Leaf
     {
 
         BulletInterface()
         {
-   
             m_collisionConfiguration.reset( new btDefaultCollisionConfiguration() );
 
             m_dispatcher.reset( new btCollisionDispatcher(m_collisionConfiguration.get() ) );
-       
-            m_broadphase.reset( new btDbvtBroadphase() );
-         
-            sol.reset( new btSequentialImpulseConstraintSolver() );
-       
-            m_dynamicsWorld.reset( new btDiscreteDynamicsWorld( m_dispatcher.get(), m_broadphase.get(), sol.get(), m_collisionConfiguration.get() ) );
-       
-             m_dynamicsWorld->setGravity(btVector3(0,-10,0));
-             
-             // Add the ground
-             groundShape.reset( new btBoxShape(btVector3(btScalar(50.),btScalar(50.),btScalar(50.))) );
-             m_collisionShapes.push_back(groundShape.get());
-      
-             // Align it to the world
-             groundTransform.setIdentity();
-             groundTransform.setOrigin(btVector3(0,-50,0));
-             myMotionState.reset( new btDefaultMotionState(groundTransform));
-	
-             addShape();
 
-             t.begin();
+            m_broadphase.reset( new btDbvtBroadphase() );
+
+            sol.reset( new btSequentialImpulseConstraintSolver() );
+
+            m_dynamicsWorld.reset( new btDiscreteDynamicsWorld( m_dispatcher.get(), m_broadphase.get(), sol.get(), m_collisionConfiguration.get() ) );
+
+            m_dynamicsWorld->setGravity(btVector3(0,-10,0));
+
+            addShape();
+
+            t.begin();
         }
 
         L3::Timing::ChronoTimer t;
@@ -57,11 +109,6 @@ namespace Visualisers
         {
             btVector3 blue(0,0,1);
             btVector3 pos(0,0,0);
-            
-            //sDebugDraw.drawSphere(pos,10.1,blue);
-            //sDebugDraw.drawTriangle(pos,10.1,blue);
-            //sDebugDraw.draw3dText( btVector3(40,40,40), "Duck" );
-
             
             if (m_dynamicsWorld)
             {
@@ -74,115 +121,45 @@ namespace Visualisers
 
             doQuery();
 
+            for( std::list< L3::Visualisers::Leaf* >::iterator it = leafs.begin();
+                    it != leafs.end();
+                    it++ )
+                (*it)->onDraw3D( g );
+
             t.begin();
+           
         }
 
         void addShape()
         {
-
-            btScalar mass(0);
-
-            btVector3 localInertia(0,0,0);
-
-            btRigidBody::btRigidBodyConstructionInfo rbInfo( mass, myMotionState.get(), groundShape.get(), localInertia);
-            //btRigidBody* body = new btRigidBody(rbInfo);
-            boost::shared_ptr< btRigidBody > body( new btRigidBody(rbInfo) );
-            body->setRollingFriction(1);
-            body->setFriction(1);
-            //add the body to the dynamics world
-            m_dynamicsWorld->addRigidBody(body.get());
-
-
-            bodies.push_back( body );
-
-            btVector3 quad[] = {
-                btVector3(0,1,-1),
-                btVector3(0,1,1),
-                btVector3(0,-1,1),
-                btVector3(0,-1,-1)};
-
-
-            btTriangleMesh* mesh = new btTriangleMesh();
-            mesh->addTriangle(quad[0],quad[1],quad[2],true);
-            mesh->addTriangle(quad[0],quad[2],quad[3],true);
-
-            btBvhTriangleMeshShape* trimesh = new btBvhTriangleMeshShape(mesh,true,true);
-
-            m_collisionShapes.push_back(trimesh);
-
-
-            btVector3 convexPoints[]={ btVector3(-1,-1,-1),btVector3(-1,-1,1),btVector3(-1,1,1),btVector3(-1,1,-1),
-            btVector3(2,0,0)};
-
-
-            btCollisionShape* colShapes[5] = {
-                new btConvexHullShape(&convexPoints[0].getX(),sizeof(convexPoints)/sizeof(btVector3),sizeof(btVector3)),
-                new btSphereShape(1),
-                new btCapsuleShape(0.2,1),
-                new btCylinderShape(btVector3(0.2,1,0.2)),
-                new btBoxShape(btVector3(1,1,1))
-            };
-
-
-            for (int i=0;i<5;i++)
-            {
-                btTransform startTransform;
-                startTransform.setIdentity();
-                startTransform.setOrigin(btVector3((i-3)*5,1,0));
-
-
-                m_collisionShapes.push_back(colShapes[i]);
-
-                btRigidBody::btRigidBodyConstructionInfo rbInfo(mass,0,colShapes[i],localInertia);
-                rbInfo.m_startWorldTransform = startTransform;
-                btRigidBody* body = new btRigidBody(rbInfo);
-                body->setRollingFriction(0.03);
-                body->setFriction(1);
             
-                m_dynamicsWorld->addRigidBody(body);
-
-            }
-
+            // Add Box
+            BoxRenderer* renderer = new L3::Visualisers::BoxRenderer();
+            leafs.push_back( renderer );
+            m_dynamicsWorld->addRigidBody( renderer->box_body );
         }
 
         void doQuery()
         {
-                static float up = 0.f;
-            static float dir = 1.f;
-
-            //add some simple animation
-            up+=0.01*dir;
-
-            if (btFabs(up)>2)
-            {
-                dir*=-1.f;
-            }
-
-            btTransform tr = m_dynamicsWorld->getCollisionObjectArray()[1]->getWorldTransform();
-            static float angle = 0.f;
-            angle+=0.01f;
-            tr.setRotation(btQuaternion(btVector3(0,1,0),angle));
-            m_dynamicsWorld->getCollisionObjectArray()[1]->setWorldTransform(tr);
-   
             m_dynamicsWorld->updateAabbs();
             m_dynamicsWorld->computeOverlappingPairs();
 
             btVector3 red(1,0,0);
-            
             {
-                btVector3 from(-30,1+up,0);
-                btVector3 to(30,1,0);
+                btVector3 from(-30,-30,30);
+                btVector3 to(0,0,0);
                 sDebugDraw.drawLine(from,to,btVector4(0,0,0,1));
+                
                 btCollisionWorld::AllHitsRayResultCallback allResults(from,to);
+                
                 allResults.m_flags |= btTriangleRaycastCallback::kF_KeepUnflippedNormal;
                 m_dynamicsWorld->rayTest(from,to,allResults);
 
                 for (int i=0;i<allResults.m_hitFractions.size();i++)
                 {
                     btVector3 p = from.lerp(to,allResults.m_hitFractions[i]);
-                    sDebugDraw.drawSphere(p,10,red);
+                    sDebugDraw.drawSphere(p,1,red);
                 }
-
             }
         }
 
@@ -205,6 +182,8 @@ namespace Visualisers
         boost::shared_ptr< btCollisionShape > groundShape;
              
         btTransform groundTransform;
+   
+        std::list< L3::Visualisers::Leaf* > leafs;
     };
 }
 }
