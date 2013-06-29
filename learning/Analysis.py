189c8d8913ea026a3697c3ce0538ee71740f346b
Ian Baldwin <ian@iabaldwin.com>
2013-06-29 12:35:56 -0700

    Updates

--- learning/Analysis.py
+++ learning/Analysis.py
@@ -20,14 +20,7 @@ class Generator:
 
         self.clouds = []
 
-        #self.figure, self.axes = pyplot.figure()                
-        self.figure = pyplot.figure()                
-      
-        self.axes = self.figure.add_subplot(111)
-
-        #Create figure
-        self.scatter = pyplot.scatter( numpy.random.rand(541,1), numpy.random.rand(541,1), c='b' )
-
+        
         for index, threaded_elements in enumerate( threaded ):
 
             pt_cloud = Dataset.Projector.Project( threaded_elements )
@@ -37,16 +30,26 @@ class Generator:
             if ( index > 20 ):
                 break
 
+        self.figure = pyplot.figure()                
+        self.axes = self.figure.add_subplot(111)
+
+
+    def Plot(self):
+
+        # Create figure
+        self.scatter = pyplot.scatter( [], [], c='tomato' )
+
+        # Create animation
         anim = animation.FuncAnimation(self.figure, self.plot_helper, frames=len(self.clouds) )
 
+        # Run
         pyplot.show()
 
     def plot_helper( self, i ):
-
+        
+        # Extract XY
         dat = self.clouds[i].pts[ 0::, 0::2 ]
-
-        c = numpy.random.random((3, dat.shape[0] ))
-
+        
         self.scatter.set_offsets( dat )
 
         mins = numpy.amin( dat, axis=0 )
@@ -55,12 +58,10 @@ class Generator:
         self.axes.set_xlim( [mins[0], maxs[0] ] )
         self.axes.set_ylim( [mins[1], maxs[1] ] )
 
-        self.scatter = self.axes.scatter(dat[ 0::, 0 ], dat[ 0::, 1 ], c="tomato", s=20, animated=True)
-        
         return self.scatter,
 
 
 
 if __name__=="__main__":
 
-    G = Generator( '/Users/ian/code/datasets/2012-04-16-20-05-30NightWoodstock1/' )
+    G = Generator( '/Users/ian/code/datasets/2012-04-16-20-05-30NightWoodstock1/' ).Plot()
