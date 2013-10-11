358a7471eb0ae6b5d64a6308ae93be273246be64
Ian Baldwin <ian@iabaldwin.com>
2013-10-10 21:11:10 -0700

    Updates

--- core/Utils.cpp
+++ core/Utils.cpp
@@ -2,40 +2,35 @@
 
 namespace L3
 {
-namespace Utils
-{
-namespace Math
-{
-
-double degreesToRadians( double degrees )
-{
-        return (M_PI/180.0)*degrees;
-}
-
-double radiansToDegrees( double radians )
-{
-        return (180.0/M_PI)*radians;
-}
-
-
-L3::SE3 poseFromRotation( const Eigen::Matrix4f& mat )
+    namespace Utils
     {
-        double x = mat(0, 3 );
-        double y = mat(1, 3 );
-        double z = mat(2, 3 );
-    
-        double q = atan2( mat(1,0), mat( 0, 0 ) );
-        double r = atan2( -1* mat(2, 0), sqrt( pow( mat(2,1),2 )+ pow( mat(2,2) ,2) ) );
-        double p = atan2( mat(2,1), mat(2,2) );
+        namespace Math
+        {
 
-        return L3::SE3( x,y,z,r,p,q );
+            double degreesToRadians( double degrees )
+            {
+                return (M_PI/180.0)*degrees;
+            }
 
-    }
-
-} // Math
-
+            double radiansToDegrees( double radians )
+            {
+                return (180.0/M_PI)*radians;
+            }
 
 
+            L3::SE3 poseFromRotation( const Eigen::Matrix4f& mat )
+            {
+                double x = mat(0, 3 );
+                double y = mat(1, 3 );
+                double z = mat(2, 3 );
 
-} // Utils
+                double q = atan2( mat(1,0), mat( 0, 0 ) );
+                double r = atan2( -1* mat(2, 0), sqrt( pow( mat(2,1),2 )+ pow( mat(2,2) ,2) ) );
+                double p = atan2( mat(2,1), mat(2,2) );
+
+                return L3::SE3( x,y,z,r,p,q );
+            }
+
+        } // Math
+    } // Utils
 } // L3
