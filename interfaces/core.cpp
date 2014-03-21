7f44a565b86df4518d76764c0dc45db2218e6945
Ian Baldwin <ian@iabaldwin.com>
2014-03-21 15:12:12 -0700

    Interfaces

--- interfaces/core.cpp
+++ interfaces/core.cpp
@@ -1,15 +1,31 @@
 #include <L3.h>
 #include <Core.h>
+#include <Windower.h>
+#include <Iterator.h>
 
 #include <boost/python.hpp>
+#include <boost/noncopyable.hpp>
 
-void run(void)
+boost::shared_ptr<int> run()
 {
-    boost::shared_ptr< L3::Dataset > dataset( new L3::Dataset(""));
+    return boost::shared_ptr<int>( new int(10) );
 }
 
 BOOST_PYTHON_MODULE(Core)
 {
     using namespace boost::python;
-    def("run", run);
+  
+    def( "run", run );
+
+    //class_<L3::Dataset, boost::noncopyable>( "Dataset", init<std::string>() )
+        //.def( "validate", &L3::Dataset::validate )
+        //.def( "load", &L3::Dataset::load )
+        //.def( "path", &L3::Dataset::path )
+        //.def( "name", &L3::Dataset::name );
+
+    class_<L3::SlidingWindow<L3::SE3>, boost::noncopyable, boost::shared_ptr< L3::SlidingWindow<L3::SE3> > >( "SlidingWindow",init<std::string, float>()  );
+
+    //class_< L3::ConstantTimeIterator<L3::SE3>, boost::shared_ptr< L3::ConstantTimeIterator<L3::SE3> >, boost::shared_ptr< L3::SlidingWindow<L3::SE3> > >( "ConstantTimeIterator", init< boost::shared_ptr< L3::SlidingWindow<L3::SE3> > >());
+    //class_< L3::ConstantTimeIterator<double> >( "ConstantTimeIterator" )
+        //.def( init< boost::shared_ptr< L3::SlidingWindow<double> >, int>() );
 }
