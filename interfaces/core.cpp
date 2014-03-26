f1554d7659af865d808692106a437633681a0039
Ian Baldwin <ian@iabaldwin.com>
2014-03-26 10:22:59 -0700

    Fixed boost::python & shard_ptr issue

--- interfaces/core.cpp
+++ interfaces/core.cpp
@@ -17,15 +17,17 @@ BOOST_PYTHON_MODULE(Core)
   
     def( "run", run );
 
-    //class_<L3::Dataset, boost::noncopyable>( "Dataset", init<std::string>() )
-        //.def( "validate", &L3::Dataset::validate )
-        //.def( "load", &L3::Dataset::load )
-        //.def( "path", &L3::Dataset::path )
-        //.def( "name", &L3::Dataset::name );
+    class_<L3::Dataset, boost::noncopyable>( "Dataset", init<std::string>() )
+        .def( "validate", &L3::Dataset::validate )
+        .def( "load", &L3::Dataset::load )
+        .def( "path", &L3::Dataset::path )
+        .def( "name", &L3::Dataset::name );
 
-    class_<L3::SlidingWindow<L3::SE3>, boost::noncopyable, boost::shared_ptr< L3::SlidingWindow<L3::SE3> > >( "SlidingWindow",init<std::string, float>()  );
+    class_<L3::SE3, boost::shared_ptr<L3::SE3> >( "SE3" );
 
-    //class_< L3::ConstantTimeIterator<L3::SE3>, boost::shared_ptr< L3::ConstantTimeIterator<L3::SE3> >, boost::shared_ptr< L3::SlidingWindow<L3::SE3> > >( "ConstantTimeIterator", init< boost::shared_ptr< L3::SlidingWindow<L3::SE3> > >());
+    class_<L3::SlidingWindow<L3::SE3>, boost::shared_ptr< L3::SlidingWindow<L3::SE3> >, boost::noncopyable >( "SlidingWindow", init<std::string,double>() );
+
+    //class_< L3::ConstantTimeIterator<L3::SE3>, boost::shared_ptr< L3::ConstantTimeIterator<L3::SE3> > > >( "ConstantTimeIterator", init< boost::shared_ptr< L3::SlidingWindow<L3::SE3> > >());
     //class_< L3::ConstantTimeIterator<double> >( "ConstantTimeIterator" )
         //.def( init< boost::shared_ptr< L3::SlidingWindow<double> >, int>() );
 }
