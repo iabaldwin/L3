167c60c0cdb5a725ccb62cc1dee39d8721f64adf
Ian Baldwin <ian@zoox.com>
2017-04-03 15:22:33 -0700

    Removed unnecessary source command

--- run_demo.sh
+++ run_demo.sh
@@ -4,7 +4,6 @@ export L3_DIR=`pwd`
 
 if [ -e ../L3.build/ ]; then
     cd ../L3.build/
-    #source environment.sh
     ./renderer/tests/test_L3 ~/datasets/oxford/2012-04-16-20-05-30NightWoodstock1/
 else
     echo 'Build directory does not exist'
